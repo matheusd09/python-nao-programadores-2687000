e7867a39547eb2a8bdd4a704d38b93911e6d0c3b
matheusd09 <35032439+matheusd09@users.noreply.github.com>
2024-07-07 21:26:29 +0000

    Resolução dos Exercicios

--- exercicios/1-primeiros-passos/1-variaveis.py
+++ exercicios/1-primeiros-passos/1-variaveis.py
@@ -4,8 +4,12 @@
 # - E não podemos usar palavras reservadas do Python, como if, for, from, etc. 
 
 # Declare variáveis do tipo string
+nome = 'fulano'
 
 # Declare variáveis de cada tipo numérico
-
+numero_inteiro = 1
+numero_float = 1.5
 
 # Declare uma variável do tipo booleano
+verdadeiro = True
+falso = False
--- exercicios/1-primeiros-passos/3-numerico.py
+++ exercicios/1-primeiros-passos/3-numerico.py
@@ -3,9 +3,22 @@ idade_numerica = 46
 altura = 1.74
 
 # Descubra o tipo de dado de cada variável acima
-
+print(type(data_nascimento))
+print(type(idade_numerica))
+print(type(altura))
 
 # Realize uma operação entre dados do tipo string e inteiro
+idade = 2024-1991
+print(f'Tenho {idade} anos de idade')
+
 
 
 # Realize uma operação entre dados do tipo inteiro e float
+soma = idade + (7 / 12)
+print(soma)
+print(int(soma))
+print(round(soma,2))
+
+##Repetição
+nome = "Fulano"
+print(nome * 3) 
