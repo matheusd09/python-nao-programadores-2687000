b21d29917da3494e66044987c7921c2da23cbcd8
matheusd09 <35032439+matheusd09@users.noreply.github.com>
2024-07-07 23:21:56 +0000

    Exercicios Operadores e Lista

--- exercicios/1-primeiros-passos/4-operadores.py
+++ exercicios/1-primeiros-passos/4-operadores.py
@@ -2,9 +2,22 @@ ano_nascimento = 1989
 ano_formatura = 2010
 
 # Considerando que as variáveis acima correspondem a 'Gerlaine', descubra a idade dela no ano da sua formatura
-
+idade_formatura = ano_formatura - ano_nascimento
+print(f'Gerlaine possuía {idade_formatura} anos quando formou.')
 
 # Escreva expressões comparativas usando os operadores relacionais >, <= e ==. Imprima na tela as respostas
-
+a = 2
+b = 50
+b > a   # True
+b >= a  # True
+b <= a   # False
+b == a   # False
+'python' != 'javascript' # True (compara o comprimento da string)
 
 # Crie expressões comparativas mais complexas utilizando operadores lógicos and, or e not. Imprima na tela as respostas
+3 == 3 and 2 == 2 #True
+3 == 3 and 2 == 1 #False
+3 == 3 or 2 == 2  #True
+3 == 3 or 2 == 1  #True
+not 3 == 1        #True
+not 3 == 3        #False
--- exercicios/2-estruturas-de-dados/1-lista.py
+++ exercicios/2-estruturas-de-dados/1-lista.py
@@ -1,13 +1,38 @@
 # Crie uma lista apenas com elementos numéricos
+numerica = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 # Crie uma lista contendo todos os tipos e estrutura de dados que você aprendeu até agora
+mista = ['python', 4, [1,2,3], True, False]
 
 # Imprima na tela apenas os 5 primeiros elementos da lista
+print(numerica[0:5])
+print(mista[0:5])
 
 # Crie um slice na lista para que imprima na tela os elementos de índice par
+print(numerica[0:-1:2])
 
 # Remova da lista o último item
+print(numerica.pop())
 
 # Insira na lista um novo item
+numerica.append(15)
+print(numerica)
 
 # Remova da lista um item específico
+numerica.remove(11)
+print(numerica)
+
+#----------------------#
+##Manipulação de Lista
+lista = ['vazia']
+lista2 = numerica
+
+lista2.extend(mista)
+print(lista)
+print(lista2)
+
+mista[4] = 1.5
+print(mista)
+print(mista)
+
+
